8f85f2fa306eabe2a4f9838258ec43c468bbc583
Lucas Yamamoto <17356081+lucasayb@users.noreply.github.com>
2024-10-23 00:50:28 -0400

    adds api and base structure

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
+from fastapi import FastAPI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from langserve import add_routes
 
 import os
 
 load_dotenv()
 
+# I don't want to start the server 
+# without an API Key, so I really 
+# want an error to be returned
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 prompt = ChatPromptTemplate.from_template("""
-You are an write that receives a list of 
-top trending GitHub Repositories and creates
-a markdown article with that. You will receive
-the data and in return will give as output
-a title with a content.                                       
+You are an writer that receives a list of 
+top trending GitHub Repositories from a RSS feed 
+and creates a markdown article with that, being 
+the top 10 trending repositories of the week.
+You will receive the data as the context and 
+in return you will give me the article built, 
+in a natural way, but without too much gibberish.
+
+<context>
+{context}
+</context>
 """)
 
 class Article(BaseModel):
     title: str = Field(description="Title of the article")
     content: str = Field(description="Markdown details of the article")
 
-llm = ChatOpenAI(api_key=OPENAI_API_KEY)
+llm = ChatOpenAI(api_key=OPENAI_API_KEY, model="gpt-4o-mini")
 
 parser = JsonOutputParser(pydantic_object=Article)
 
 chain = prompt | llm | parser
+
+app = FastAPI(
+    title="LangChain Server",
+    version="1.0",
+    description="A simple API server to save articles in the Lucas Yamamoto's blog."
+)
+
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    
+    uvicorn.run(app, host="localhost", port=8000)
